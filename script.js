8ee34e6f904567e5a2e5f31f7d03358d3cce4656
ssamdeveloper <ssamdeveloper2025@gmail.com>
2025-08-17 18:18:54 +0800

    initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+$(function(){
+
+$('.ssam-community').slick({
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    arrows: false,
+    adaptiveHeight: false,
+});
+
+});
